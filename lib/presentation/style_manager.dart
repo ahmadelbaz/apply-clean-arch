1f091030957f07fd0e6b15ed9a786c3b8ff8233b
ahmadelbaz <ahmeed.elbaz@gmail.com>
2023-04-30 08:36:46 +0300

    Created style manager for all textStyles in presentation layer

---
+++ lib/presentation/style_manager.dart
@@ -0,0 +1,40 @@
+import 'package:apply_mvvm_arch/presentation/fonts_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontFamily: FontsConstants.fontFamily,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      color: color);
+}
+
+// Light style
+TextStyle getLightTextStyle(
+    {double fontSize = FontSizes.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeights.light, color);
+}
+
+// Medium style
+TextStyle getMediumTextStyle(
+    {double fontSize = FontSizes.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeights.medium, color);
+}
+
+// Regular style
+TextStyle getRegularTextStyle(
+    {double fontSize = FontSizes.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeights.regular, color);
+}
+
+// SemiBold style
+TextStyle getSemiBoldTextStyle(
+    {double fontSize = FontSizes.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeights.semiBold, color);
+}
+
+// Bold style
+TextStyle getBoldTextStyle(
+    {double fontSize = FontSizes.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeights.bold, color);
+}
